2f242c606c7c013e9fc7fd9e3e0afb19cebbb6e6
caidewu <caidewu555@gmail.com>
2023-09-22 00:12:49 +0800

    项目初始化

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+const {Solar, Lunar, HolidayUtil} = require('lunar-javascript')
+
+console.log(Lunar.fromDate(new Date()).toFullString())
+console.log(Solar.fromYmd(2016, 1, 1).toFullString())
+
+console.log(HolidayUtil.getHoliday(2020, 5, 2) + '')
+App({})
